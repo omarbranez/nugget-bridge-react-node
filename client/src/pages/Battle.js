ecbff8ebf3ba97707e7435fc906dd212628d3dd7
Omar <omar.branez@gmail.com>
2022-03-11 23:52:05 -0500

    stub in front and back sprites and position them

--- client/src/pages/Battle.js
+++ client/src/pages/Battle.js
@@ -1,11 +1,11 @@
 const Battle = () => {
     return (
         <div className="battle" style={{position: "relative"}}>
-            <div style={{position:"absolute", top: "50px", right: "100px"}}>
-                <span>cpu pokemon</span>
+            <div style={{position:"absolute", top: "0vh", right: "15vw"}}>
+                <img src='/6.png' width="225%"/>
             </div>
-            <div style={{position:"absolute", bottom: "150px", left: "100px"}}>
-                <span>player pokemon</span>
+            <div style={{position:"absolute", bottom: "8vh", left: "5vw"}}>
+                <img src='/3.png' width="225%"/>
             </div>
         </div>
     )
